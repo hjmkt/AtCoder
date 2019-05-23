e17c114394ac7a7fc98ddaab9ed7b0d3fe5b23dc
hjmkt <rachmaninov3126@gmail.com>
2019-05-23 21:31:50 +0900

    Add arc012

---
+++ ARC/012/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string day;
+    cin >> day;
+    if(day=="Monday") cout << 5 << endl;
+    else if(day=="Tuesday") cout << 4 << endl;
+    else if(day=="Wednesday") cout << 3 << endl;
+    else if(day=="Thursday") cout << 2 << endl;
+    else if(day=="Friday") cout << 1 << endl;
+    else if(day=="Saturday") cout << 0 << endl;
+    else if(day=="Sunday") cout << 0 << endl;
+}
