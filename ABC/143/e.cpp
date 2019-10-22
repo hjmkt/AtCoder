57b5f78380ffa33723006d085255d709a8e72d5e
hjmkt <rachmaninov3126@gmail.com>
2019-10-22 21:29:16 +0900

    Update abc143

--- ABC/143/e.cpp
+++ ABC/143/e.cpp
@@ -4,12 +4,21 @@ using namespace std;
 using ll = long long;
 using vll = vector<ll>;
 using vvll = vector<vll>;
+using vvvll = vector<vvll>;
 
 #define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
 #define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
 #define rep(i, n) REP(i, 0, n)
 #define irep(i, n) IREP(i, n, 0)
 #define all(v) v.begin(), v.end()
+
+template<typename T=ll>
+void warshall(vector<vector<T>> &v, function<bool(const T&, const T&)> comp=less<T>()){
+    ll n = v.size();
+    rep(i, n) rep(j, n) rep(k, n){
+        v[j][k] = comp(v[j][k], v[j][i]+v[i][k])? v[j][k] : (v[j][i]+v[i][k]);
+    }
+}
 
 int main(){
     ll n, m, l;
@@ -26,15 +35,11 @@ int main(){
     vll s(q), t(q);
     rep(i, q) cin >> s[i] >> t[i];
 
-    rep(i, n) rep(j, n) rep(k, n){
-        d[j][k] = min(d[j][k], d[j][i]+d[i][k]);
-    }
+    warshall(d);
     rep(i, n) rep(j, n){
         d[i][j] = i==j? 0 : d[i][j]<=l? 1 : INT_MAX;
     }
-    rep(i, n) rep(j, n) rep(k, n){
-        d[j][k] = min(d[j][k], d[j][i]+d[i][k]);
-    }
+    warshall(d);
 
     rep(i, q){
         ll res = d[s[i]-1][t[i]-1];
