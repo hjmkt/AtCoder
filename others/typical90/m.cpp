c4f71eb4406d8f637980eef9a4594b44d0b0f04b
hjmkt <hjmkt3126@gmail.com>
2021-10-16 17:58:21 +0900

    Update typical90

---
+++ others/typical90/m.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+template<typename T=ll>
+pair<vector<ll>, vector<ll>> djkstra(vector<unordered_map<ll, T>> &e, ll src, function<bool(const T&, const T&)> comp=greater<T>()){
+    auto c = [&comp](tuple<T, ll, ll> l, tuple<T, ll, ll> r){ return comp(get<0>(l), get<0>(r)); };
+    priority_queue<tuple<T, ll, ll>, vector<tuple<T, ll, ll>>, decltype(c)> q(c);
+    vector<T> s(e.size(), LLONG_MAX);
+    vector<ll> prev(e.size(), -1);
+    q.push({0, src, -1});
+    while(!q.empty()){
+        auto [d, n, p] = q.top();
+        q.pop();
+        if(s[n]<LLONG_MAX) continue;
+        s[n] = d;
+        prev[n] = p;
+        for(auto t : e[n]){
+            if(s[t.first]<LLONG_MAX) continue;
+            q.push({d+t.second, t.first, n});
+        }
+    }
+    return {s, prev};
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, M;
+    cin >> N >> M;
+    vll A(M), B(M), C(M);
+    rep(i, M) cin >> A[i] >> B[i] >> C[i];
+
+    vector<unordered_map<ll, ll>> e(N);
+    rep(i, M) {
+        e[A[i]-1][B[i]-1] = C[i];
+        e[B[i]-1][A[i]-1] = C[i];
+    }
+
+    auto fwd = djkstra(e, 0);
+    auto bwd = djkstra(e, N-1);
+
+    rep(i, N){
+        ll d = fwd.first[i] + bwd.first[i];
+        cout << d << endl;
+    }
+}
