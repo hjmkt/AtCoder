3e191eeff677e1c3eb397c61c710971264460c76
hjmkt <hjmkt3126@gmail.com>
2022-05-28 20:40:24 +0900

    Update typical90: bz.cpp

---
+++ others/typical90/bz.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, M;
+    cin >> N >> M;
+    vll a(M), b(M);
+    rep(i, M) cin >> a[i] >> b[i];
+    vll count(N, 0);
+    rep(i, M){
+        if(a[i]<b[i]) ++count[b[i]-1];
+        else ++count[a[i]-1];
+    }
+    ll ans = 0;
+    rep(i, N){
+        if(count[i]==1) ++ans;
+    }
+    cout << ans << endl;
+}
