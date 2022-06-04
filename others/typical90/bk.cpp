0b6d0b1ba830eb7c6c1778c5b7b6aa4f7d10e384
hjmkt <hjmkt3126@gmail.com>
2022-06-04 17:01:32 +0900

    Update typical90: bk.cpp

---
+++ others/typical90/bk.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll H, W;
+    cin >> H >> W;
+    vvll P(H, vll(W, 0));
+    rep(i, H) rep(j, W) cin >> P[i][j];
+
+    ll ans = 0;
+
+    REP(i, 1, 1<<H){
+        vll rows;
+        rep(j, H){
+            if((i>>j)&1) rows.push_back(j);
+        }
+        vll count(H*W+1, 0);
+        rep(j, W){
+            bool ok = true;
+            ll n = P[rows[0]][j];
+            REP(k, 1, rows.size()){
+                if(P[rows[k]][j]!=n){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) ++count[n];
+        }
+        ll mcol = 0;
+        rep(j, H*W+1) mcol = max(mcol, count[j]);
+        ans = max(ans, mcol*((ll)rows.size()));
+    }
+    cout << ans << endl;
+}
