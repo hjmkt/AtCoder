52191d6bef6072aa993531c1a36cde267bf95331
Hajime Kato <hjmkt3126@gmail.com>
2022-10-14 12:02:55 +0900

    Update abc272: c.cpp

---
+++ ABC/abc272/c.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cwchar>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N;
+    cin >> N;
+    vll A(N);
+    rep(i, N) cin >> A[i];
+    vll odds, evens;
+    rep(i, N) {
+        if(A[i]%2==0) evens.push_back(A[i]);
+        else odds.push_back(A[i]);
+    }
+    sort(all(evens), greater<ll>());
+    sort(all(odds), greater<ll>());
+    ll ans = -1;
+    if(evens.size()>=2){
+        ans = max(ans, evens[0]+evens[1]);
+    }
+    if(odds.size()>=2){
+        ans = max(ans, odds[0]+odds[1]);
+    }
+    cout << ans << endl;
+}
