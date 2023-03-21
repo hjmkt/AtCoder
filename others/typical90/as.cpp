dbe3a9fd74b0712d1aa9418a1ec4b8ab035b8929
Hajime Kato <hjmkt3126@gmail.com>
2023-03-21 23:51:16 +0900

    Update typical90: as.cpp

---
+++ others/typical90/as.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, K;
+    cin >> N >> K;
+    vll X(N), Y(N);
+    rep(i, N) cin >> X[i] >> Y[i];
+
+    vvll dp(N, vll(1<<N, -1));
+    dp[0][0] = 0;
+    REP(i, 1, (1<<N)+1){
+        ll n = 0;
+        rep(j, N){
+            if(((i>>j)&1)==1) ++n;
+        }
+        REP(j, n, K+1){
+            dp[i][j] = max(dp[i][j], dp[i-1][j]);
+        }
+    }
+}
