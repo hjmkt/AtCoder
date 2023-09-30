4e2db728f2b9d52d7a148f923c6a90cff4b38e05
Hajime Kato <hjmkt3126@gmail.com>
2023-09-30 22:28:12 +0900

    Add abc321

---
+++ ABC/abc321/a.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N;
+    cin >> N;
+
+    ll d = 0;
+    ll n = N;
+    while(n>0){
+        ++d;
+        n /= 10;
+    }
+    bool ok = true;
+    ll last = -1;
+    rep(i, d){
+        ll x = N % 10;
+        if(last >= x){
+            ok = false;
+            break;
+        }
+        N /= 10;
+        last = x;
+    }
+    cout << (ok ? "Yes" : "No") << endl;
+}
