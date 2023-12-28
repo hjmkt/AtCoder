d7a72d3c805629a2ff78ef2a15fdc8c4b21d3cd7
Hajime Kato <hjmkt3126@gmail.com>
2023-12-28 19:58:17 +0900

    Update abc332: c.cpp

---
+++ ABC/abc332/c.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, M;
+    cin >> N >> M;
+    string S;
+    cin >> S;
+
+    ll a = M, ar = M;
+    ll b = 0, br = 0;
+
+    rep(i, N){
+        if(S[i]=='0'){
+            ar = M;
+            br = b;
+        }
+        else if(S[i]=='1'){
+            if(ar>0) --ar;
+            else if(br>0) --br;
+            else ++b;
+        }
+        else{
+            if(br>0) --br;
+            else ++b;
+        }
+    }
+    cout << b << endl;
+}
