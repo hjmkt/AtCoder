fa304792d7ec7c6776878e11e714aa278b4a7b49
Hajime Kato <hjmkt3126@gmail.com>
2024-01-06 23:45:03 +0900

    Update abc333: b.cpp

---
+++ ABC/abc333/b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    string S, T;
+    cin >> S >> T;
+
+    ll s = abs((ll)S[0] - (ll)S[1]);
+    ll t = abs((ll)T[0] - (ll)T[1]);
+    if(s==t || s==5-t) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
