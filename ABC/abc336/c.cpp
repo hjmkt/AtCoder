270078d75fdb39a445b4b593190dfc2a3dd84125
Hajime Kato <hjmkt3126@gmail.com>
2024-01-19 00:37:51 +0900

    Update abc336: c.cpp

---
+++ ABC/abc336/c.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N;
+    cin >> N;
+
+    ll b = 1;
+    ll n = 1;
+    while(b*5<N){
+        b *= 5;
+        ++n;
+    }
+    string ans = "";
+    --N;
+    rep(i, n){
+        ll t = N/b * 2;
+        ans += '0' + t;
+        N -= b * (t / 2);
+        b /= 5;
+    }
+    if(ans.empty()) ans = "0";
+    cout << ans << endl;
+}
