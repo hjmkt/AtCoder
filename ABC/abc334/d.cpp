fa13423a877b6ae78a5dc91ab5191d61291f1d26
Hajime Kato <hjmkt3126@gmail.com>
2024-01-26 00:03:57 +0900

    Update abc334: d.cpp

---
+++ ABC/abc334/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, Q;
+    cin >> N >> Q;
+    vll R(N);
+    rep(i, N) cin >> R[i];
+
+    sort(all(R));
+    vll S(N+1, 0);
+    rep(i, N) S[i+1] = S[i] + R[i];
+
+    rep(i, Q){
+        ll X;
+        cin >> X;
+        ll l = 0, r = N;
+        while(l<r){
+            ll m = (l+r+1) / 2;
+            if(S[m]>X) r = m - 1;
+            else l = m;
+        }
+        cout << l << endl;
+    }
+}
