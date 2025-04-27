078ba8428161b349c051ee60f4cf59106b55f635
Hajime Kato <hjmkt3126@gmail.com>
2025-04-28 02:39:04 +0900

    Update abc373: b.cpp

---
+++ ABC/abc373/b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    string S;
+    cin >> S;
+
+    umap<char, ll> mp;
+    rep(i, S.size()){
+        mp[S[i]] = i;
+    }
+    ll ans = 0;
+    char last = 'A';
+    REP(i, 1, S.size()){
+        char c = (char)('A' + i);
+        ll d = (ll)mp[c] - (ll)mp[last];
+        d = abs(d);
+        ans += d;
+        last = c;
+    }
+    cout << ans << endl;
+}
