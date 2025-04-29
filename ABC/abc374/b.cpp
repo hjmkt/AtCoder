5b9b371302f99ad180c242348ba82f4410827f58
Hajime Kato <hjmkt3126@gmail.com>
2025-04-30 00:38:27 +0900

    Update abc374: b.cpp

---
+++ ABC/abc374/b.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    string S, T;
+    cin >> S >> T;
+    if(S==T) cout << 0 << endl;
+    else{
+        ll ans = min(S.size(), T.size()) + 1;
+        rep(i, min(S.size(), T.size())){
+            if(S[i] != T[i]){
+                ans = i + 1;
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+}
