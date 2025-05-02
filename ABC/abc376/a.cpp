d846d79dd54322b451582e78b2ab712525fb0b3b
Hajime Kato <hjmkt3126@gmail.com>
2025-05-03 01:07:45 +0900

    Add abc376

---
+++ ABC/abc376/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, C;
+    cin >> N >> C;
+    vll T(N);
+    rep(i, N) cin >> T[i];
+
+    ll last_t = -10000;
+    ll ans = 0;
+    rep(i, N){
+        if(T[i]>=last_t+C){
+            ++ans;
+            last_t = T[i];
+        }
+    }
+    cout << ans << endl;
+}
