cb98456ed7db2a4614c8853d2aa9d949e271b53c
Hajime Kato <hjmkt3126@gmail.com>
2025-05-08 10:33:23 +0900

    Add abc380

---
+++ ABC/abc380/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    string N;
+    cin >> N;
+    ll n1 =0, n2 =0, n3 =0;
+    rep(i, N.size()){
+        if(N[i] == '1') n1++;
+        else if(N[i] == '2') n2++;
+        else if(N[i] == '3') n3++;
+    }
+    if(n1 == 1 && n2 == 2 && n3 == 3) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
