a1c6f43f660cbf2b11ebffedc562ea920b6cfcb2
Hajime Kato <hjmkt3126@gmail.com>
2025-06-22 14:07:52 +0900

    Update abc392: b.cpp

---
+++ ABC/abc392/b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vvvll = vector<vvll>;
+template<class T> using uset = unordered_set<T>;
+template<class T, class U> using umap = unordered_map<T, U>;
+template<class T> using mset = multiset<T>;
+template<class T, class U> using mmap = multimap<T, U>;
+template<class T> using umset = unordered_multiset<T>;
+template<class T, class U> using ummap = unordered_multimap<T, U>;
+
+#define REP(i, n, m) for(ll i=n; i<(ll)m; ++i)
+#define IREP(i, n, m) for(ll i=n-1; i>=m; --i)
+#define rep(i, n) REP(i, 0, n)
+#define irep(i, n) IREP(i, n, 0)
+#define all(v) v.begin(), v.end()
+#define vprint(v) for(auto e:v){cout<<e<<" ";};cout<<endl;
+#define vvprint(vv) for(auto v:vv){vprint(v)};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << setprecision(20);
+
+    ll N, M;
+    cin >> N >> M;
+    vll A(M);
+    rep(i, M) cin >> A[i];
+
+    vector<bool> ans(N+1, true);
+    rep(i, M) {
+        ans[A[i]] = false;
+    }
+    ll count = 0;
+    REP(i, 1, N+1) {
+        if(ans[i]) count++;
+    }
+    cout << count << endl;
+    REP(i, 1, N+1) {
+        if(ans[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
